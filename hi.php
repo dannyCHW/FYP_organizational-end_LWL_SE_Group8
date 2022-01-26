64a5bc08cecc06b34a95fbffd15517e60213d17b
dannyCHW <dannydt1697@gmail.com>
2022-01-26 23:24:24 +0800

    Testing

--- hi.php
+++ hi.php
@@ -1,23 +1,37 @@
-<?php 
+<?php
+require_once 'vendor/autoload.php';
+
 use Google\Cloud\Firestore\FirestoreClient;
 
-/**
- * Initialize Cloud Firestore with default project ID.
- */
-function setup_client_create(string $projectId = null)
-{
-    // Create the Cloud Firestore client
-    if (empty($projectId)) {
-        // The `projectId` parameter is optional and represents which project the
-        // client will act on behalf of. If not supplied, the client falls back to
-        // the default project inferred from the environment.
-        $db = new FirestoreClient();
-        printf('Created Cloud Firestore client with default project ID.' . PHP_EOL);
+$projectId = 'clean-algebra-325515';
+
+$db = new FirestoreClient([
+    'projectId' => $projectId,
+]);
+
+/*
+$docRef = $db->collection('set1')->document('doc1');
+$snapshot = $docRef->snapshot();
+
+if ($snapshot->exists()) {
+    printf('Document data:' . PHP_EOL);
+    //print_r($snapshot->data());
+    $tmp = [];
+    $tmp = $snapshot->data();
+    print_r($tmp);
+} else {
+    printf('Document %s does not exist!' . PHP_EOL, $snapshot->id());
+} 
+*/
+
+$docRef = $db->collection('set1');
+$documents = $docRef->documents();
+foreach ($documents as $document) {
+    if ($document->exists()) {
+        printf('Document data for document %s:' . PHP_EOL, $document->id());
+        print_r($document->data());
+        printf(PHP_EOL);
     } else {
-        $db = new FirestoreClient([
-            'projectId' => $projectId,
-        ]);
-        printf('Created Cloud Firestore client with project ID: %s' . PHP_EOL, $projectId);
+        printf('Document %s does not exist!' . PHP_EOL, $document->id());
     }
 }
-?>
