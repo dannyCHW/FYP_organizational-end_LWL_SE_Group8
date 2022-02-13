043957fb077d1d730208737af64f1b71d8b4ca39
simonhkg2002@gmail.com <simonhkg2002@gmail.com>
2022-02-14 01:14:56 +0800

    testing dashboard

---
+++ bar/buttom.php
@@ -0,0 +1,31 @@
+<?php
+  echo"
+    <script src='Adminjs/vendor/jquery-1.12.4.min.js'></script>
+    <script src='Adminjs/bootstrap.min.js'></script>
+    <script src='Adminjs/wow.min.js'></script>
+    <script src='Adminjs/jquery-price-slider.js'></script>
+    <script src='Adminjs/jquery.meanmenu.js'></script>
+    <script src='Adminjs/owl.carousel.min.js'></script>
+    <script src='Adminjs/jquery.sticky.js'></script>
+    <script src='Adminjs/jquery.scrollUp.min.js'></script>
+    <script src='Adminjs/counterup/jquery.counterup.min.js'></script>
+    <script src='Adminjs/counterup/waypoints.min.js'></script>
+    <script src='Adminjs/counterup/counterup-active.js'></script>
+    <script src='Adminjs/scrollbar/jquery.mCustomScrollbar.concat.min.js'></script>
+    <script src='Adminjs/scrollbar/mCustomScrollbar-active.js'></script>
+    <script src='Adminjs/metisMenu/metisMenu.min.js'></script>
+    <script src='Adminjs/metisMenu/metisMenu-active.js'></script>
+    <script src='Adminjs/morrisjs/raphael-min.js'></script>
+    <script src='Adminjs/morrisjs/morris.js'></script>
+    <script src='Adminjs/morrisjs/morris-active.js'></script>
+    <script src='Adminjs/sparkline/jquery.sparkline.min.js'></script>
+    <script src='Adminjs/sparkline/jquery.charts-sparkline.js'></script>
+    <script src='Adminjs/sparkline/sparkline-active.js'></script>
+    <script src='Adminjs/calendar/moment.min.js'></script>
+    <script src='Adminjs/calendar/fullcalendar.min.js'></script>
+    <script src='Adminjs/calendar/fullcalendar-active.js'></script>
+    <script src='Adminjs/plugins.js'></script>
+    <script src='Adminjs/main.js'></script>
+    <script src='Adminjs/tawk-chat.js'></script>
+  "
+ ?>
---
+++ bar/toolbars.php
@@ -0,0 +1,39 @@
+<?php
+echo"<div class='left-sidebar-pro'>
+      <nav id='sidebar' class=''>
+          <div class='sidebar-header'>
+              <a href='indexHTML.php'><img class='main-logo' src='img/logo/logo.png' alt='' /></a>
+              <strong><a href='index.html'><img src='img/logo/logosn.png' alt='' /></a></strong>
+          </div>
+          <div class='left-custom-menu-adp-wrap comment-scrollbar'>
+              <nav class='sidebar-nav left-sidebar-menu-pro'>
+                  <ul class='metismenu' id='menu1'>
+                      <li class='active'>
+                          <a class='has-arrow' href='indexHTML.php'>
+                             <span class='educate-icon educate-home icon-wrap'></span>
+                             <span class='mini-click-non'>Analytics</span>
+                          </a>
+                          <ul class='submenu-angle' aria-expanded='true'>
+                              <li><a title='Dashboard v.1' href='indexHTML.php'><span class='mini-sub-pro'>Dashboard</span></a></li>
+                              <li><a title='Analytics' href='analytics.html'><span class='mini-sub-pro'>Analytics</span></a></li>
+                          </ul>
+                      </li>
+                      <li>
+                          <a class='has-arrow' href='all-courses.html' aria-expanded='false'><span class='educate-icon educate-library icon-wrap'></span> <span class='mini-click-non'>Verify</span></a>
+                          <ul class='submenu-angle' aria-expanded='false'>
+                              <li><a title='All Library' href='library-assets.html'><span class='mini-sub-pro'>Accounts Verify</span></a></li>
+                              <li><a title='Add Library' href='add-library-assets.html'><span class='mini-sub-pro'>Events Verify</span></a></li>
+                          </ul>
+                      </li>
+                      <li>
+                          <a title='Landing Page' href='events.html' aria-expanded='false'><span class='educate-icon educate-student icon-wrap'></span><span class='mini-click-non'>Management</span></a>
+                      </li>
+                      <li>
+                          <a title='Landing Page' href='events.html' aria-expanded='false'><span class='educate-icon educate-event icon-wrap sub-icon-mg' aria-hidden='true'></span> <span class='mini-click-non'>Event</span></a>
+                      </li>
+                  </ul>
+              </nav>
+          </div>
+      </nav>
+  </div>"
+ ?>
